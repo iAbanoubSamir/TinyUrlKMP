70ef341625c011fa4ddbbab3dbd47f3c06ba3018
Abanoub Samir <abanoub.sameer.22@gmail.com>
2025-02-12 19:53:31 +0200

    Add required dependencies

---
+++ composeApp/src/desktopMain/kotlin/com/tiny/url/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.tiny.url
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
