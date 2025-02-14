7c0c442abd69062d7d58c703d917f053a40143ef
Abanoub Samir <abanoub.sameer.22@gmail.com>
2025-02-14 18:12:34 +0200

    Remove dummy code from wizard

--- composeApp/src/commonMain/kotlin/com/tiny/url/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.tiny.url
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/tiny/url/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.tiny.url
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
