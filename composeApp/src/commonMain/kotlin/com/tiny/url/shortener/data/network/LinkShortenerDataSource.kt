f158dc954b6e44e8988e560f81111de214d3fae7
Abanoub Samir <abanoub.sameer.22@gmail.com>
2025-02-15 17:05:35 +0200

    Implement link shortener datasource

---
+++ composeApp/src/commonMain/kotlin/com/tiny/url/shortener/data/network/LinkShortenerDataSource.kt
@@ -0,0 +1,28 @@
+package com.tiny.url.shortener.data.network
+
+import com.tiny.url.core.data.safeCall
+import com.tiny.url.core.domain.DataError
+import com.tiny.url.core.domain.Result
+import com.tiny.url.shortener.data.dto.LinkResponseDto
+import io.ktor.client.HttpClient
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+
+private const val BASE_URL = "https://api.tinyurl.com"
+
+class LinkShortenerDataSource(
+    private val httpClient: HttpClient
+) {
+    suspend fun shortenLink(
+        originalLink: String
+    ): Result<LinkResponseDto, DataError.Remote> {
+        return safeCall {
+            httpClient.post(
+                urlString = "$BASE_URL/create"
+            ) {
+                parameter("apiToken", "YOUR_API_KEY")
+                parameter("url", originalLink)
+            }
+        }
+    }
+}
