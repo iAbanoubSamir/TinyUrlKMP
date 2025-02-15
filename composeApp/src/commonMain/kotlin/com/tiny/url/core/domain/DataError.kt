c71eb0316d0421b01464f8b9d262c44ee77158e4
Abanoub Samir <abanoub.sameer.22@gmail.com>
2025-02-15 17:04:20 +0200

    Create data error types

---
+++ composeApp/src/commonMain/kotlin/com/tiny/url/core/domain/DataError.kt
@@ -0,0 +1,20 @@
+package com.tiny.url.core.domain
+
+sealed interface DataError : Error {
+    enum class Remote : DataError {
+        UNAUTHORIZED,
+        METHOD_NOT_ALLOWED,
+        INVALID_URL,
+        TOO_MANY_REQUESTS,
+        REQUEST_TIMEOUT,
+        NO_INTERNET,
+        SERVER,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local : DataError {
+        DISK_FULL,
+        UNKNOWN
+    }
+}
